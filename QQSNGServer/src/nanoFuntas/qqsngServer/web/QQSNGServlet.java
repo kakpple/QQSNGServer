0cbff6a9bba274adfda5dc1cb6f0501dffb0cb8b
zheyujin <kimcherwoo@gmail.com>
2012-12-08 18:10:56 +0900

    Source refactored.

--- QQSNGServer/src/nanoFuntas/qqsngServer/web/QQSNGServlet.java
+++ QQSNGServer/src/nanoFuntas/qqsngServer/web/QQSNGServlet.java
@@ -13,7 +13,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import nanoFuntas.qqsngServer.DataHandler;
+import nanoFuntas.qqsngServer.Util;
+import nanoFuntas.qqsngServer.bll.DataHandler;
 
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
@@ -24,135 +25,130 @@ import org.json.simple.JSONValue;
 @WebServlet("/QQSNGServlet")
 public class QQSNGServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
+
 	static private boolean DEBUG = true;
 	static private String TAG = "QQSNGServlet";
-    	
-	/**
-     * Default constructor. 
-     */
-    public QQSNGServlet() {
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		PrintWriter out = response.getWriter();	
+		PrintWriter out = response.getWriter();
 		out.write("1");
 	}
-	
+
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		if(DEBUG) System.out.println(TAG + ": doPost()");						
-				
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		if (DEBUG)
+			System.out.println(TAG + ": doPost()");
+
 		// get JSON request parameters handle it and finally send it
-		JSONObject jsonReq = getJsonReq(request);		
-		JSONObject jsonRsp = DataHandler.handleData(jsonReq);		
+		JSONObject jsonReq = getJsonReq(request);
+		JSONObject jsonRsp = DataHandler.handleData(jsonReq);
 		sendJsonRsp(response, jsonRsp);
 	}
-	
+
 	/**
 	 * Function sendStrRsp sends String data to client as response
 	 * 
-	 * @response this parameter is passed from HttpServletResponse response in function doPost()
+	 * @response this parameter is passed from HttpServletResponse response in
+	 *           function doPost()
 	 * @strToSend String data to be sent to client
 	 */
-	static private void sendStrRsp(HttpServletResponse response, String strToSend) {
-		if(DEBUG) System.out.println(TAG + ": sendStrRsp()");						
-		
+	static private void sendStrRsp(HttpServletResponse response,
+			String strToSend) {
+		if (DEBUG)
+			System.out.println(TAG + ": sendStrRsp()");
+
 		OutputStream outStrm = null;
 		ObjectOutputStream objOutStrm = null;
-		
+
 		// write date to be sent to client
-		try{
+		try {
 			outStrm = response.getOutputStream();
 			objOutStrm = new ObjectOutputStream(outStrm);
 			objOutStrm.writeObject(strToSend);
 			objOutStrm.flush();
-		} catch (IOException e){
+		} catch (Exception e) {
 			e.printStackTrace();
-		} catch (Exception e){
-			e.printStackTrace();
-		} finally{
-			// release resources
-			try{
-				if(outStrm != null) outStrm.close();
-				if(objOutStrm != null) objOutStrm.close();
-			} catch (Exception e){
-				e.printStackTrace();
-			}
+		} finally {
+			Util.safeClose(outStrm, objOutStrm);
 		}
 	}
-		
+
 	/**
-	 * Function getStrReq extracts String data from request(HttpServletRequest request) parameter in doPost function, and return it.
+	 * Function getStrReq extracts String data from request(HttpServletRequest
+	 * request) parameter in doPost function, and return it.
 	 * 
-	 * @request, this parameter is passed from HttpServletRequest request in function doPost()
+	 * @request, this parameter is passed from HttpServletRequest request in
+	 *           function doPost()
 	 * @return, return String extracted from client request
 	 */
 	static private String getStrReq(HttpServletRequest request) {
-		if(DEBUG) System.out.println(TAG + ": getStrReq()");						
-		
-		String strReq = null;		
+		if (DEBUG)
+			System.out.println(TAG + ": getStrReq()");
+
+		String strReq = null;
 		InputStream inStrm = null;
 		ObjectInputStream objInStrm = null;
-		
+
 		// get date from client
 		try {
 			inStrm = request.getInputStream();
 			objInStrm = new ObjectInputStream(inStrm);
 			strReq = (String) objInStrm.readObject();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e){			
+		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			// release resources
-			try{
-				if(inStrm != null) inStrm.close();
-				if(objInStrm != null) objInStrm.close();
-			} catch(Exception e){
-				e.printStackTrace();
-			}
+			Util.safeClose(inStrm, objInStrm);
 		}
 
 		return strReq;
 	}
 
 	/**
-	 * Function sendJsonRsp sends JSONObject data to client and wraps function sendStrRsp.
+	 * Function sendJsonRsp sends JSONObject data to client and wraps function
+	 * sendStrRsp.
 	 * 
-	 * @response, this parameter is passed from HttpServletResponse response in function doPost()
+	 * @response, this parameter is passed from HttpServletResponse response in
+	 *            function doPost()
 	 * @jsonToSend, JSONObject data to be sent to client
 	 */
 	private void sendJsonRsp(HttpServletResponse response, JSONObject jsonToSend) {
-		if(DEBUG) System.out.println(TAG + ": sendJsonRsp()");						
-		
+		if (DEBUG)
+			System.out.println(TAG + ": sendJsonRsp()");
+
 		String jsonStr = null;
 		jsonStr = jsonToSend.toString();
 		sendStrRsp(response, jsonStr);
-	}	
-	
+	}
+
 	/**
-	 * Function getJsonReq extracts JSONObject data from request(HttpServletRequest request) parameter in doPost function, and return it.
-	 * Function getJsonReq wraps function getStrReq.
+	 * Function getJsonReq extracts JSONObject data from
+	 * request(HttpServletRequest request) parameter in doPost function, and
+	 * return it. Function getJsonReq wraps function getStrReq.
 	 * 
-	 * @request, this parameter is passed from HttpServletRequest request in function doPost()
+	 * @request, this parameter is passed from HttpServletRequest request in
+	 *           function doPost()
 	 * @return, return JSONObject extracted from client request
 	 */
-	private JSONObject getJsonReq(HttpServletRequest request){
-		if(DEBUG) System.out.println(TAG + ": getJsonReq()");						
-		
+	private JSONObject getJsonReq(HttpServletRequest request) {
+		if (DEBUG)
+			System.out.println(TAG + ": getJsonReq()");
+
 		String strReq = null;
 		JSONObject jsonReq = null;
 		strReq = getStrReq(request);
 		jsonReq = (JSONObject) JSONValue.parse(strReq);
-		
+
 		return jsonReq;
 	}
 }
